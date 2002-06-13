f9db0a6adb0d3ddc9f8bcbab0d3df7f9d914f90f
(no author) <(no author)@9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52>
2002-06-13 10:35:15 +0000

    This commit was manufactured by cvs2svn to create tag 'yaws-0-48'.
    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws@80 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

---
+++ tags/yaws-0-48/yaws/src/yaws.hrl
@@ -0,0 +1,60 @@
+%%%----------------------------------------------------------------------
+%%% File    : yaws.hrl
+%%% Author  : Claes Wikstrom <klacke@hyber.org>
+%%% Purpose : 
+%%% Created : 16 Jan 2002 by Claes Wikstrom <klacke@hyber.org>
+%%%----------------------------------------------------------------------
+
+-author('klacke@hyber.org').
+-include_lib("yaws/include/yaws_api.hrl").
+-include("yaws_debug.hrl").
+
+%% global conf
+-record(gconf,{file,
+	       yaws_dir,
+	       trace,
+	       debug,
+	       logdir,
+	       ebin_dir = [],
+	       keepalive_timeout = 15000,
+	       max_num_cached_files = 400,
+	       max_num_cached_bytes = 1000000,  %% 1 MEG
+	       max_size_cached_file = 8000,
+	       default_type = "text/html",
+	       timeout = 2000,
+	       include_dir = [],
+	       yaws   %% FIXME add version here 
+	         }).  %% a list of lists of #sconfs
+                      %% one list of #sconf's per listen ip
+
+
+-record(ssl, 
+	{
+	 keyfile,
+	 certfile,
+	 verify = 0,
+	 depth = 1,
+	 password,
+	 cacertfile,
+	 ciphers,
+	 cachetimeout}).
+
+
+%% server conf
+-record(sconf,
+	{port = 8000,
+	 docroot,
+	 access_log = true,
+	 listen = {127,0,0,1},
+	 servername = "localhost",
+	 default_server_on_this_ip = true,
+	 ets,
+	 ssl,
+	 authdirs = []
+	}).
+
+
+
+-record(dcc, {
+	  doclose = true,
+	  chunked = false}).
