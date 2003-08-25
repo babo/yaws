cd61cf5e92a78c868f86de79d13960e81554192c
(no author) <(no author)@9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52>
2003-08-25 21:09:21 +0000

    This commit was manufactured by cvs2svn to create tag 'yaws-1-30'.
    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws@500 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

---
+++ tags/yaws-1-30/yaws/src/mime_type_c.erl
@@ -0,0 +1,123 @@
+%%%----------------------------------------------------------------------
+%%% File    : mime_type_c.erl
+%%% Author  : Claes Wikstrom <klacke@hyber.org>
+%%% Purpose : 
+%%% Created : 10 Jul 2002 by Claes Wikstrom <klacke@hyber.org>
+%%%----------------------------------------------------------------------
+
+-module(mime_type_c).
+-author('klacke@hyber.org').
+-compile(export_all).
+
+%% this module reads the mime.types file and creates
+%% the mod mime_types.erl
+%% default type is text/plain
+
+compile() ->
+    R = (catch c()),
+    io:format("~p~n ",[R]),
+    erlang:halt().
+
+
+c() ->
+    {ok, F} = file:open("mime.types", [read]),
+    io:format("Compiling mime.types ... > mime_types.erl ~n", []),
+    T = ets:new(aa, [set, public]),
+    c(F, T, io:get_line(F, '')).
+
+c(F, T, eof) ->
+    file:close(F),
+    gen(T);
+c(F, T, {error, terminated}) ->
+    file:close(F),
+    gen(T);
+c(F, T, Line) ->
+    case Line of
+	[$#|_] ->
+	    ignore;
+	[$\s|_ ] ->
+	    ignore;
+	L ->
+	    case string:tokens(L, [$\s, $\t]) of
+		[] ->
+		    ignore;
+		[_] ->  
+		    ignore;
+		[MimeType | Exts] ->
+		    lists:foreach(
+		      fun(E) ->
+			      ets:insert(T, {E, MimeType}),
+			      ets:insert(T, {up(E), MimeType})
+		      end, Exts)
+	    end
+    
+    end,
+    c(F, T, io:get_line(F, '')).
+
+up(L) ->
+    lists:map(fun(C) -> upper(C) end, L).
+upper(C) ->
+    if
+	C >= $a, C =< $z ->
+	    C - ($a - $A);
+	true ->
+	    C
+    end.
+
+nonl([]) ->
+    [];
+nonl([$\n|T]) ->
+    nonl(T);
+nonl([H|T]) ->
+    [H|nonl(T)].
+
+special(Fd, Ext, Type) ->
+    io:format(Fd, "t(~p) -> {~p, ~p};~n", 
+	      [Ext, Type, "text/html"]).
+    
+
+revspecial(Fd, Ext, Type) ->
+    io:format(Fd, "revt(~p) -> {~p, ~p};~n", 
+	      [lists:reverse(Ext), Type, "text/html"]).
+    
+
+gen(T) ->
+    {ok, Fd} = file:open("mime_types.erl", [write]),
+    io:format(Fd, 
+	      "-module(mime_types). ~n"
+	      "-compile(export_all). ~n", []),
+   
+    L = lists:sort(ets:tab2list(T)),
+    special(Fd, "yaws", yaws),
+    special(Fd, "php", php),
+    special(Fd, "cgi", cgi),
+    special(Fd, "PHP", php),
+    special(Fd, "CGI", cgi),
+    lists:foreach(
+      fun({Ext, MT}) ->
+	      io:format(Fd, "t(~p) -> {regular, ~p};~n", [nonl(Ext), MT])
+      end, L),
+    io:format(Fd, "t(_) -> {regular, \"text/plain\"}.~n~n~n", []),
+
+
+
+    revspecial(Fd, "yaws", yaws),
+    revspecial(Fd, "php", php),
+    revspecial(Fd, "cgi", cgi),
+    revspecial(Fd, "PHP", php),
+    revspecial(Fd, "CGI", cgi),
+    lists:foreach(
+      fun({Ext, MT}) ->
+	      io:format(Fd, "revt(~p) -> {regular, ~p};~n", 
+			[nonl(lists:reverse(Ext)), MT])
+      end, L),
+    io:format(Fd, "revt(_) -> {regular, 
+                               \"text/plain\"}.~n~n~n", []).
+
+    
+		  
+		 
+			  
+
+
+		      
